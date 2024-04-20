4e7e7106c6ce42cac66749b1d9f68f9fc6c87db2
Kennard Sugirotok <kennardfx@gmail.com>
2024-04-20 20:37:06 +0700

    fix: moved all routes into routes.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
-import { NextFunction, Request, Response } from "express"
 import dotenv from 'dotenv'
 import express from 'express'
 import passport from 'passport'
-import { isLoggedIn } from "./middleware"
-
 const session = require('express-session')
 
 require('./auth')
@@ -22,35 +19,6 @@ app.use(passport.initialize())
 app.use(passport.session())
 
 const routes = require('./routes')
-
-app.get('/', (req: Request, res: Response) => {
-    res.send('<a href="/auth/google">Authenticate</a>')
-})
-
-app.get('/auth/google', passport.authenticate('google', { scope: ['email', 'profile'] }))
-
-app.get('/auth/google/callback', passport.authenticate('google', {
-    successRedirect: '/protected',
-    failureRedirect: '/auth/failure',
-}))
-
-app.get('/logout', function (req, res, next) {
-    req.logout(function (err) {
-        if (err) { return next(err); }
-        res.redirect('/');
-    });
-})
-
-app.get('/auth/failure', (req: Request, res: Response) => {
-    res.send('Something went wrong!')
-    res.redirect('/')
-})
-
-app.get('/protected', isLoggedIn, (req: Request, res: Response) => {
-    console.log(req);
-    res.send(req.user)
-})
-
 
 app.use('/', routes)
 
--- src/routes.ts
+++ src/routes.ts
@@ -3,11 +3,42 @@ import { HelloService } from './services/hello';
 import { UploadVideo } from './services/videos/UploadVideoService';
 import { AttachAnalyticsToRecord, CreateRecord, GetAllRecords, GetOneRecord } from './services/records/recordService';
 import { CreateAnalytics, GetAllAnalytics, GetOneAnalytics } from './services/analytics/analyticsService';
+import { isLoggedIn } from "./middleware"
+import passport from 'passport';
+
 const express = require('express');
 const router = express.Router();
 
 // Add this middleware to parse JSON bodies
 router.use(express.json());
+
+router.get('/', (req: Request, res: Response) => {
+    res.send('<a href="/auth/google">Authenticate</a>')
+})
+
+router.get('/auth/google', passport.authenticate('google', { scope: ['email', 'profile'] }))
+
+router.get('/auth/google/callback', passport.authenticate('google', {
+    successRedirect: '/protected',
+    failureRedirect: '/auth/failure',
+}))
+
+router.get('/logout', function (req: Request, res: Response, next: NextFunction) {
+    req.logout(function (err) {
+        if (err) { return next(err); }
+        res.redirect('/');
+    });
+})
+
+router.get('/auth/failure', (req: Request, res: Response) => {
+    res.send('Something went wrong!')
+    res.redirect('/')
+})
+
+router.get('/protected', isLoggedIn, (req: Request, res: Response) => {
+    console.log(req);
+    res.send(req.user)
+})
 
 router.get('/hi', HelloService);
 
